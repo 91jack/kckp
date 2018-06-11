d62e30a5ff7b539c84f960f34b9396829bbb2160
91jack <2764132626@qq.com>
2018-06-11 10:49:33 +0800

    1
    1

--- static/js/app.js
+++ static/js/app.js
@@ -10,6 +10,10 @@
     onWindowResize();
 })();
 
+// 获取本地时间
+
+
+
 // 返回上一页
 $('#goback').on('click', function(){
 	window.history.back();
@@ -17,7 +21,78 @@ $('#goback').on('click', function(){
 
 // 首页 index.html
 
+// 登录
+// 获取验证码
+		
+$('#getcode').on('click',function(){
+	$.ajax({
+	    type: "POST",
+	    url: yzcodeUrl,
+	  	data:{
+			phone:$('#phone').val(),
+			type:'4'
+		},
+	    success: function(data){
+	   		//服务器返回响应，根据响应结果，分析是否登录成功；
+			if(data.status == 2000){
+				console.log(data)
+				$('#yzcode').val(data.obj)
+			}else{
+				console.log(data)
+			}
+	    }
+	})
+})	
 
+// 登录
+$('#login').on('click',function(){
+	$.ajax({
+	    type: "POST",
+	    url: loginUrl,
+	  	data:{
+			loginType:'2',
+			phoneType:'2',
+			userName:$('#phone').val(),
+			yzcode: $('#yzcode').val()
+		},
+	    success: function(data){
+	   		//服务器返回响应，根据响应结果，分析是否登录成功；
+			if(data.status == 2000){
+				console.log(data)
+				
+				// localstorge.token
+				window.location.href = 'step2.html'
+				
+				
+			}else{
+				console.log(data)
+			}
+	    }
+	})	
+})
+
+
+// step4.html 
+// 创建事故
+$('#createAccident').on('click', function(){
+	 
+	$.ajax({
+	   type: "POST",
+	   url: createAccidentUrl,
+	   data: {
+	   	type:'2',
+	   	token:'3661363439356430613064343464366539306566306664353466313962643034',
+	   },
+	   success: function(data){
+	   	console.log(res)
+	   	// 
+	   }
+	})
+	 
+});
+
+
+//
 
 
 
